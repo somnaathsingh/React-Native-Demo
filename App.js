fd246742f00003c6f5f9be919feeb211131439a6
SOMNAATH SINGH <somnathsingh5013@gmail.com>
2023-06-20 10:44:12 +0530

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//web 392535976541-6vq7hequf4dd9fp37kuqv2srrljemhuv.apps.googleusercontent.com
+// android 392535976541-u9t0qp9jap45e8a3b9bup8g7ic1vgp7g.apps.googleusercontent.com
+
+
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+import ProductCard from './unauth_product';
+import AuthProductCard from './auth_product';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
+
+  const [request, response, promptAsync] = Google.useAuthRequest(
+    {
+      expoClientId:'392535976541-ir6vr8ohiitvjns77lemp7l8vnjq9tg8.apps.googleusercontent.com',
+      webClientId: '392535976541-6vq7hequf4dd9fp37kuqv2srrljemhuv.apps.googleusercontent.com',
+      androidClientId: '392535976541-u9t0qp9jap45e8a3b9bup8g7ic1vgp7g.apps.googleusercontent.com',
+      scopes: ['openid', 'profile', 'email'],
+    },
+    { authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth' }
+  );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { access_token } = response.params;
+      fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+        headers: { Authorization: `Bearer ${access_token}` },
+      })
+        .then((response) => response.json())
+        .then((userInfo) => {
+          setUser(userInfo);
+        })
+        .catch((error) => {
+          console.log('Error fetching user info:', error);
+        });
+    }
+  }, [response]);
+
+  const handleLogin = async () => {
+    try {
+      promptAsync();
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  };
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+    <View>
+      <Text style={styles.text}>
+        Product Card for Unauthorised Users
+      </Text>
+      <ProductCard/>
+      </View>
+      {user!=null ? (
+        <View>
+          <Text style={styles.text}>Product Card for Authorised Users</Text>
+          <AuthProductCard/>
+          <Button title="Logout" style={styles.button} onPress={handleLogout}/>
+        </View>
+      ) : (
+        <Button title="Login with Google" onPress={handleLogin} styles={styles.button} />
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  text: {
+    marginTop:30,
+    fontSize:20,
+    fontWeight:'bold',
   },
+  image: {
+    padding:50,
+    width:400
+  }
 });
---
+++ auth_product.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet } from 'react-native';
+
+
+const AuthProductCard = ({ title, price, imageSource }) => {
+  return (
+    <View style={styles.container}>
+      <Image source='https://akm-img-a-in.tosshub.com/indiatoday/images/story/202211/iphone_15_1-one_one_0.jpg?VersionId=9SXvA92iozGaChGDPBb7I.rynGQB14eI' style={styles.image} />
+      <Text style={styles.title}>IPhone</Text>
+      <Text style={styles.price}>Price:Rs 110000</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: '40%',
+    height: 200,
+    marginBottom: 12,
+    borderRadius: 8,
+    marginLeft:12
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  price: {
+    fontSize: 16,
+    color: '#888888',
+  },
+});
+
+export default AuthProductCard;
